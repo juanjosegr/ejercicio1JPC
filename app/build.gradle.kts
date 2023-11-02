9f7d5152eff6d92030812e33894e94d62306ec0e
juanj <juanjosegr9@gmail.com>
2023-11-02 01:42:42 +0100

    Ejercicio 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.constraintlayout:constraintlayoutcompose:1.0.1")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha07")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
--- app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.ejercicio1
 
-import android.graphics.ColorSpace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,16 +10,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.colorspace.Rgb
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ejercicio1.ui.theme.Ejercicio1Theme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    myRow()
+                    Ejercicio1()
                 }
             }
         }
@@ -54,13 +56,75 @@ class MainActivity : ComponentActivity() {
         }
 
     }
-    @Preview (showBackground = true)
-    @Composable
-    fun myRow(){
-        Row (
-            modifier = Modifier.horizontalScroll(rememberScrollState()).fillMaxSize().background(Color.hsl(0.35f,0.60f,0.70f,0.65f))
-        ){
 
+    @Composable
+    fun Ejercicio1(){
+        Row (verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .horizontalScroll(rememberScrollState())
+                .fillMaxSize()
+                .background(Color.hsl(0.35f, 0.60f, 0.70f, 0.65f))
+        ){
+            Text(
+                "Caja 1",
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .background(color = Color.Yellow)
+                    .wrapContentHeight(Alignment.CenterVertically)
+                ,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 16.sp,
+            )
+            Text(
+                "Caja 2",
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .background(color = Color.Black)
+                    .wrapContentHeight(Alignment.CenterVertically)
+                ,
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                fontSize = 16.sp,
+            )
+            Text(
+                "Caja 3",
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .background(color = Color.Cyan)
+                    .wrapContentHeight(Alignment.CenterVertically)
+                ,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 16.sp,
+            )
+            Text(
+                "Caja 4",
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .background(color = Color.Green)
+                    .wrapContentHeight(Alignment.CenterVertically)
+                ,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 16.sp,
+            )
+            Text(
+                "Caja 5",
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .background(color = Color.Red)
+                    .wrapContentHeight(Alignment.CenterVertically)
+                ,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 16.sp,
+            )
         }
     }
 
